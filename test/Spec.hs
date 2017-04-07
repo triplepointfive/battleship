c66e3f7627ebf8878281fb046ea6651b43e63964
Ilya Smelkov <ismelkov@restream.rt.ru>
2017-04-07 18:56:37 +0300

    Hspec ♨

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,9 @@
+import Test.Hspec
+
+import Warships.Types
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+  describe "hitCell" $ do
+    it "Cell becomes miss if was empty" $ do
+      hitCell Empty `shouldBe` Miss
