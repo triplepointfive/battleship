3b3e04a945a53dc1a9c48575a4f4312f44993805
Ilya Smelkov <ismelkov@restream.rt.ru>
2017-04-07 19:05:13 +0300

    Introducing: State 💍

--- src/Warships/Types.hs
+++ src/Warships/Types.hs
@@ -3,18 +3,22 @@ module Warships.Types where
 
 import qualified Data.Map as Map
 
+import Control.Monad.State
+
 type ShipID = Int
 
-data Cell = Empty | HasShip ShipID | Miss | Hit ShipID | Killed ShipID
+data Cell = Empty | Hidden ShipID | Open | Injured ShipID | Killed ShipID
   deriving (Show, Eq)
 
 hitCell :: Cell -> Cell
-hitCell Empty = Miss
-hitCell (HasShip shipID) = Hit shipID
+hitCell Empty = Open
+hitCell (Hidden shipID) = Injured shipID
 hitCell x = x
 
 
-type Grid = Map.Map (Int, Int) Cell
+type Pos = (Int, Int)
+
+type Grid = Map.Map Pos Cell
 
 data Field = Field { fieldGrid :: !Grid }
 
@@ -22,8 +26,13 @@ data Field = Field { fieldGrid :: !Grid }
 -- fieldDone Field{..} = all shipSunk fieldShips
 
 
+data Action = Hit | Miss | Kill
 
+click :: Pos -> Field -> (Action, Field)
+click pos = runState (clickOn pos)
 
+clickOn :: Pos -> State Field Action
+clickOn = undefined
 
 type GameID = Int
 
--- test/Spec.hs
+++ test/Spec.hs
@@ -6,4 +6,4 @@ main :: IO ()
 main = hspec $ do
   describe "hitCell" $ do
     it "Cell becomes miss if was empty" $ do
-      hitCell Empty `shouldBe` Miss
+      hitCell Empty `shouldBe` Open
