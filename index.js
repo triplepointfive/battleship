cd88cc4fcd480f3d119d55f85d18043b98d8ab30
Ilya Smelkov <ismelkov@restream.rt.ru>
2017-05-03 18:10:14 +0300

    Updates

--- index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const React = require("react");
-const ReactDOM = require("react-dom");
-const MainMenu_1 = require("./components/MainMenu");
-ReactDOM.render(React.createElement(MainMenu_1.MainMenu, null), document.getElementById("container"));
